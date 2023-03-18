521c2049d894f3d8e7051dbcb532cb5af2c034bc
Michael Esch <eschmichael@pm.me>
2023-03-17 23:10:27 -0400

    demonstrates message sending and receive loop
    two spawned processes talking to each other

---
+++ talk.erl
@@ -0,0 +1,25 @@
+-module(talk).
+-export([alice/0, bob/2, run/0]).
+
+alice() ->
+    receive
+        {message, PId} ->
+                io:fwrite("Alice got a message~n"),
+                PId ! message,
+                alice();
+        finished -> io:fwrite("Alice is finished~n")
+    end.
+
+bob(0, PId) ->
+    PId ! finished,
+    io:fwrite("Bob is finished\n");
+bob(N, PId) ->
+    PId ! {message, self()},
+    receive
+        message -> io:fwrite("Bob got a message~n")
+    end,
+    bob(N - 1, PId).
+
+run() ->
+    PId = spawn(talk, alice, []),
+    spawn(talk, bob, [3, PId]).
