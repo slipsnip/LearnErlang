8f5675c6664c0b33d51bf5b6d7f1411b857409cb
Michael Esch <eschmichael@pm.me>
2023-03-24 15:52:15 -0400

    set bob and alice to use nodes for networks

--- talk.erl
+++ talk.erl
@@ -1,20 +1,31 @@
 -module(talk).
--export([alice/0, bob/1, run/0]).
+-export([alice/0, bob/2, run/0, startAlice/0, startBob/1]).
 
 alice() ->
     receive
-        {message, Msg} ->
+        {message, Msg, BobNode} ->
                 io:fwrite("Alice got a message \"~s\"~n", [Msg]),
+                BobNode ! message,
                 alice();
-        finished -> done
+        finished -> io:fwrite("Alice finished~n")
     end.
 
-bob(0) ->
-    alice ! finished;
-bob(N) ->
-    alice ! {message, "The message"},
-    bob(N - 1).
+bob(0, AliceNode) ->
+    {alice, AliceNode} ! finished,
+    done;
+bob(N, AliceNode) ->
+    {alice, AliceNode} ! {message, "The message", self()},
+    receive
+        message -> io:fwrite("Bob got a reply~n")
+    end,
+    bob(N - 1, AliceNode).
 
 run() ->
     register(alice, spawn(talk, alice, [])),
     register(bob, spawn(talk, bob, [3])).
+
+startAlice() ->
+    register(alice, spawn(talk, alice, [])).
+
+startBob(AliceNode) ->
+    spawn(talk, bob, [3, AliceNode]).
